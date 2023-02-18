751f8259ceb8704c37642d9108f80d9d4a624df1
Yaksh Joshi <55596145+yakshjoshi@users.noreply.github.com>
2023-02-18 05:21:02 +0000

    Initial commit

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,61 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, solve=False):
 
+  if (len(problems) > 5):
+    return "Error: Too many problems."
 
-    return arranged_problems
+  first = ""
+  second = ""
+  lines = ""
+  sumf = ""
+  string = ""
+
+  for p in problems:
+    firstNum = p.split(" ")[0]
+    operator = p.split(" ")[1]
+    secondNum = p.split(" ")[2]
+
+    if (not firstNum.isnumeric() or not secondNum.isnumeric()):
+      return "Error: Numbers must only contain digits."
+
+    if len(firstNum) > 4 or len(secondNum) > 4:
+      return "Error: Numbers cannot be more than four digits."
+
+    if (operator == "/" or operator == "*"):
+      return "Error: Operator must be '+' or '-'."
+
+    sum = ""
+
+    if operator == "+":
+      sum = str(int(firstNum) + int(secondNum))
+
+    elif operator == "-":
+      sum = str(int(firstNum) - int(secondNum))
+
+    length = max(len(firstNum), len(secondNum)) + 2
+
+    top = str(firstNum).rjust(length)
+    bottom = str(operator) + str(secondNum).rjust(length - 1)
+    line = ""
+    ans = str(sum).rjust(length)
+
+    for l in range(length):
+      line += "-"
+
+    if p != problems[-1]:
+      first += top + " " * 4
+      second += bottom + " " * 4
+      lines += line + " " * 4
+      sumf += ans + " " * 4
+
+    else:
+      first += top
+      second += bottom
+      lines += line
+      sumf += ans
+
+  if solve:
+    string = first + "\n" + second + "\n" + lines + "\n" + sumf
+  else:
+    string = first + "\n" + second + "\n" + lines
+
+  return string
